3a3321a251a919c2c00ff85db5574add3e5fa723
Rafnes <rafzograf@gmail.com>
2025-02-17 03:21:58 +0300

    addRule refactor

--- src/main/java/bank/recommendationservice/fintech/exception/UnknownComparisonTypeException.java
+++ src/main/java/bank/recommendationservice/fintech/exception/UnknownComparisonTypeException.java
@@ -4,4 +4,8 @@ public class UnknownComparisonTypeException extends BaseBadRequestException {
     public UnknownComparisonTypeException(String s) {
         super(s);
     }
+
+    public UnknownComparisonTypeException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
