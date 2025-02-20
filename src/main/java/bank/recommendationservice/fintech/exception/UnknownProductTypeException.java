6424fca9e702ba0dfa8b8ffc5ead1123ee1a6c60
bogomolova <ibogomolllova@gmail.com>
2025-02-20 12:51:26 +0200

    + CacheServiceTest

--- src/main/java/bank/recommendationservice/fintech/exception/UnknownProductTypeException.java
+++ src/main/java/bank/recommendationservice/fintech/exception/UnknownProductTypeException.java
@@ -1,6 +1,6 @@
 package bank.recommendationservice.fintech.exception;
 
-public class UnknownProductTypeException extends BaseBadRequestException{
+public class UnknownProductTypeException extends BaseBadRequestException {
     public UnknownProductTypeException(String message) {
         super(message);
     }
